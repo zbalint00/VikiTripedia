c5493d6f6ed6a84ad3c41f98619bd604f3eeaacc
zbalint00 <dz991i@inf.elte.hu>
2026-09-17 01:53:34 +0200

    set vite base

--- VikiTripedia/vite.config.ts
+++ VikiTripedia/vite.config.ts
@@ -1,6 +1,5 @@
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
-import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
@@ -8,6 +7,6 @@ export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss()
   ],
+  base: '/vikitripedia/'
 })
